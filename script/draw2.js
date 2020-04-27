76451b3feab771f40fa3ebaac47fcb459b58f8ac
Louis Isaac DIOUF <61230778+louisisaacdiouf@users.noreply.github.com>
2020-04-27 11:14:02 +0000

    Add files via upload

---
+++ script/draw2.js
@@ -0,0 +1,105 @@
+function draw(data,annee,element){
+
+  var titre = element.append("h2").text("Modalités de l'année "+annee);
+
+  var margin = {top: 20, right: 50, bottom: 30, left: 40},
+    width = 1200 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  var x0 = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var x1 = d3.scale.ordinal();
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x0)
+    .tickSize(0)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var color = d3.scale.category10();
+
+var tooltip = d3.select("body").append("div") 
+    .attr("class", "tooltip")       
+    .style("opacity", 0);
+
+var svg = element.append("svg")
+    .attr("width", width + margin.left + margin.right )
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+  var categoriesNames = data.map(function(d) { return d.categorie; });
+  var rateNames = data[0].values.map(function(d) { return d.Indicateur; });
+
+  x0.domain(categoriesNames);
+  x1.domain(rateNames).rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(data, function(categorie) { return d3.max(categorie.values, function(d) { return d['valeur_'+annee]; }); })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .style('opacity','0')
+      .call(yAxis)
+  .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .style('font-weight','bold')
+      .text("valeur_"+annee);
+
+  svg.select('.y').transition().duration(500).delay(1300).style('opacity','1');
+
+  var slice = svg.selectAll(".slice")
+      .data(data)
+      .enter().append("g")
+      .attr("class", "g")
+      .attr("transform",function(d) { return "translate(" + x0(d.categorie) + ",0)"; });
+
+  slice.selectAll("rect")
+      .data(function(d) { return d.values; })
+  .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d) { return x1(d.Indicateur); })
+      .style("fill", function(d) { return color(d.Indicateur) })
+      .attr("y", function(d) { return y(0); })
+      .attr("height", function(d) { return height - y(0); })
+      .on("mouseover", function(d) {
+          d3.select(this).style("fill", d3.rgb(color(d.Indicateur)).darker(2));
+          tooltip.transition()    
+                .duration(200)    
+                .style("opacity", .9);    
+             tooltip.html(d.Indicateur + "<br/>"  + d['valeur_'+annee])  
+                .style("left", (d3.event.pageX) + "px")
+                .style("background", d3.rgb(color(d.Indicateur)))
+                .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+          d3.select(this).style("fill", color(d.Indicateur));
+          tooltip.transition()    
+                .duration(500)    
+                .style("opacity", 0);
+      });
+
+  slice.selectAll("rect")
+      .transition()
+      .delay(function (d) {return Math.random()*1000;})
+      .duration(1000)
+      .attr("y", function(d) { return y(d['valeur_'+annee]); })
+      .attr("height", function(d) { return height - y(d['valeur_'+annee]); });
+
+  
+}
